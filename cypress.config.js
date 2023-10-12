db3899078b1164db705c6125e11e8d2365627fbc
Arthur Petrosov <arthurpetrosov@gmail.com>
2023-10-11 22:58:19 -0700

    Add Deashboeard

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: "q5vjtc",
+  projectId: "bxqgmt",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
